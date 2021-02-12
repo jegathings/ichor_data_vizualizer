acd68f18e185378551ecfc5021ee5d18300eb5e2
James <jegathings@gmail.com>
2021-02-12 16:49:20 -0500

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './css/style.css';
+import jsonData from './data/denver_mapping_data.geojson.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  state = {
+    mapData:null,
+    cities: [],
+    displayData: null
+  }
+  componentDidMount(){
+    let aMap = new Map()
+    let tempCities = []
+    for(let feature of jsonData.features){
+      aMap.set(feature.properties.neighborhood_name, feature.properties)
+      tempCities.push(feature.properties.neighborhood_name)
+    } 
+    this.setState({mapData:aMap, cities:tempCities})      
+  }
+  displayData(data){
+    this.setState({displayData:this.state.mapData.get(data.target.innerHTML)})
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="list">
+          {
+            this.state.cities.map(city => {
+              return (
+                <>
+                  <div key={city} onClick={(event) => this.displayData(event)}>{city}</div>
+                </>
+              )
+            })
+          }
+        </div>
+        <div className="display">
+            {
+              this.state.displayData != null 
+              ?
+              Object.keys(this.state.displayData).map((key,index) => {
+                return <div>{key + "\t" + this.state.displayData[key]}</div>
+              })
+            :""
+          }
+        </div>
     </div>
   );
+}
 }
 
 export default App;
